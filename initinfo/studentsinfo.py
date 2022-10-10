70066445f2133d04c3e89b41473bed00584001b3
ppeip <wawaro@163.com>
2022-10-10 16:31:15 +0800

    feat : acconding to gpa generate stu's attendlist

--- initinfo/studentsinfo.py
+++ initinfo/studentsinfo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import random
 #定义5门课程，每个课程班级人数为90人，一学期共20次课。
 #每门课程均有5-8位同学缺席了该学期80%的课.
 #此外每次课程均还有0-3位同学由于各种原因缺席。
@@ -19,26 +20,18 @@ import pandas as pd
 #target : 
 #dataframe ： lessonno(option) studentno gpa attend01 attend02 attend03  ....... attend20
 
-
-
-
 #生成学生列表
 def gen_student(lessonno):
-    no = lessonno
     classlist =   np.array([ lessonno+i for i in range(1,91)])
     classlist = classlist[np.newaxis,:].T
     return classlist
 
-
-
-
 #按照正态分布生成gpa 
 def gen_gpa():
     gpalist = []
-    result  = np.random.normal(2.0 , 0.4 , 90)
+    result  = np.random.normal(2.2 , 0.5 , 90)
     result = result[np.newaxis,:].T
     return result
-
 
 #nparray : studentno gpa
 def merge():
@@ -53,14 +46,63 @@ def merge():
     print(res_df.dtypes)
     return res_df
 
+#计算得到一个分段线性函数 
+def getcount(x):
+    x1,y1,y2,x3,y3 = 0.8 , 18 , 14 , 4 , 0
+    x2 = (np.random.randint(14,17))/10
+    #print(x2)
+    k1 = (y1-y2)/(x1-x2)
+    b1 = y1-k1*x1
+    k2 = (2.5-y3)/(x2-x3)
+    b2 = -(k2*x3)
+    return np.piecewise(x, [x < x2 , x >= x2 ], [lambda x: k1*x+b1, lambda x: k2*x+b2])
 
-#计算得到一个分段线性函数 根据分段函数生成润的课的次数
-#然后按照次数生成润的情况的列表
+#根据分段函数生成润的课的次数
+def getattendcount(gpalist):
+    attendlist = []
+    for gpa in gpalist:
+        count = getcount(gpa)
+        if count>=0 :
+            attendlist.append(int(count))
+        else :
+            attendlist.append(0)
+    attendlist = np.array(attendlist)
+    print(attendlist)
+    return attendlist
+
+#然后按照次数生成润的情况的列表 这里生成了单个学生的到勤情况 
+def gen_stuattendlist(count):
+    list = []
+    for i in range(20):
+        if count>= 1:
+            list.append(1)
+            count-=1
+        else :
+            list.append(0)
+    list = np.array(random.sample(list,20))
+    print(list[np.newaxis,:])
+    return list[np.newaxis,:]
+
+#生成所有学生的到勤情况 然后拼接？
+def gen_allstuattendlist(attendlist):
+    res = []
+    for gpa in gpalist:
+        attinfo = gen_stuattendlist(gpa)
+        res = np.concatenate((res,attinfo),axis=1)
+        print(res.shape)
+#测试
+np.set_printoptions(precision=3,suppress=True)
+#test = np.arange(1,4,0.1)
+test = gen_gpa()
+attendlist = getattendcount(test)     
+print(gen_stuattendlist(16))
+gen_allstuattendlist(test)
+
 #然后列表和gpa拼接转换为dataframe 并和第一个表拼接的到单个班的到勤情况
 #可以生成10个班的到勤情况 前五个班作为训练集 后五个班作为测试集计算出最后的e
 df = merge()
-print(df.sort_values(by="gpa" , ascending=True)['studentno'][:8] )
+#print(df.sort_values(by="gpa" , ascending=True)['studentno'][:8] )
 
 data = df.sort_values(by="gpa" , ascending=True)['studentno'].values
 data = data[np.newaxis,:].T
-print(data)
+#print(data)
