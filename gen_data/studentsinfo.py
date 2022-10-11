6d9a1be22a6b037481ba3d9b2640e8854213bc90
ppeip <wawaro@163.com>
2022-10-11 22:49:51 +0800

    update : fix function of generate data

---
+++ gen_data/studentsinfo.py
@@ -0,0 +1,106 @@
+import numpy as np
+import pandas as pd
+import random
+
+#生成学生列表
+def gen_student(lessonno):
+    classlist =   np.array([ lessonno+i for i in range(1,91)])
+    classlist = classlist[np.newaxis,:].T
+    return classlist
+
+#按照正态分布生成gpa 
+def gen_gpa():
+    gpalist = []
+    result  = np.random.normal(2.2 , 0.6 , 90)
+    result = result[np.newaxis,:].T
+    return result
+
+#计算得到一个分段线性函数 
+def getcount(x):
+    x1,y1,y2,x3,y3 = 0.8 , 18 , 14 , 4 , 0
+    x2 = (np.random.randint(14,17))/10
+    #print(x2)
+    k1 = (y1-y2)/(x1-x2)
+    b1 = y1-k1*x1
+    k2 = (2-y3)/(x2-x3)
+    b2 = -(k2*x3)
+    return np.piecewise(x, [x < x2 , x >= x2 ], [lambda x: k1*x+b1, lambda x: k2*x+b2])
+
+#根据分段函数生成润的课的次数
+def getattendcount(gpalist):
+    attendlist = []
+    for gpa in gpalist:
+        count = getcount(gpa)
+        if count>=0 :
+            attendlist.append(int(count))
+        else :
+            attendlist.append(0)
+    attendlist = np.array(attendlist)
+    #print(attendlist)
+    return attendlist
+
+#然后按照次数生成润的情况的列表 这里生成了单个学生的到勤情况 
+def gen_stuattendlist(count):
+    list = []
+    for i in range(20):
+        if count>= 1:
+            list.append(1)
+            count-=1
+        else :
+            list.append(0)
+    '''list = np.array(random.sample(list,20))
+    print(list[np.newaxis,:])'''
+    #return list[np.newaxis,:]
+    return random.sample(list,20)
+
+#生成所有学生的到勤情况 然后拼接？
+def gen_allstuattendlist(attendlist):
+    res = []
+    for attendsumconut in attendlist:
+        temp = gen_stuattendlist(attendsumconut)
+        temp.append(attendsumconut)
+        res.append(temp)
+    return res
+
+#输入班级号生成数据
+def gen_data(lessonno):
+    np.set_printoptions(precision=3,suppress=True)
+    #生成学号
+    studentarray = gen_student(lessonno)
+    #生成gpa 列向量
+    gpaarray = gen_gpa()
+    #根据gpa生成每个学生到勤总次数
+    attendlist = getattendcount(gpaarray)
+    #根据每个学生学期到勤总次数生成这个学期的到勤情况
+    #nparray : 1-20 sumcount
+    attendchart = np.array(gen_allstuattendlist(attendlist))
+    #拼接
+    res_array = np.concatenate((studentarray,gpaarray),axis=1)
+    res_array = np.concatenate((res_array,attendchart),axis=1)
+    #print(res_array.dtype)
+    #设置显示的样式
+    #np.set_printoptions(precision=3,suppress=True)
+    col_names = ['studentno','gpa','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','count']
+    res_df = pd.DataFrame(res_array, columns=col_names)
+    res_df = res_df.infer_objects()
+    res_df['studentno'] = res_df['studentno'].astype('int')
+    res_df['count'] = res_df['count'].astype('int')
+    for lesson in [colname  for colname in col_names if colname != 'gpa']:
+        res_df[lesson] = res_df[lesson].astype('int')
+    res_df = res_df.round({'gpa': 2})
+    return res_df
+
+def gen_alldata(pathname):
+    for i in range(1,6):
+        lessonno = 2130000
+        filename = '.csv'
+        df = gen_data(lessonno + i*100)
+        df.to_csv(str(pathname + str(i)  + filename), sep = ',',index = False)
+
+if __name__ == '__main__':
+    #np.set_printoptions(precision=3,suppress=True)
+    #print(df.sort_values(by="gpa" , ascending=True)['studentno'][:8] )
+    datapath = 'data\\lessondata\\lessondata' 
+    gen_alldata(datapath)
+
+
